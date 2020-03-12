b7a3bf736d79ba4ed02b505653e44c1d8965d942
Stry87 <stry.scyth@gmail.com>
2020-03-12 10:14:04 +0100

    Debut creation memox

---
+++ app/src/main/java/com/example/memox/CreaMemox.java
@@ -0,0 +1,29 @@
+package com.example.memox;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class CreaMemox extends Activity {
+
+    Spinner dropdown;
+    String[] selectItems;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.memox_crea);
+
+        //Création du select
+
+        dropdown = findViewById(R.id.select);
+
+        selectItems = new String[]{"Coronavirus", "Anglais", "Beber"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(CreaMemox.this, android.R.layout.simple_spinner_dropdown_item, selectItems);
+        //set the spinners adapter to the previously created one.
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        dropdown.setAdapter(adapter);
+
+    }
+}
--- app/src/main/java/com/example/memox/MainActivity.java
+++ app/src/main/java/com/example/memox/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.memox;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout cL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +30,18 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        cL = findViewById(R.id.container);
         NavigationUI.setupWithNavController(navView, navController);
+
+        // Boutton pour accéder à l'activité "Créer un Memox"
+
+        final FloatingActionButton fButton = findViewById(R.id.floatingActionButton);
+        fButton.setOnClickListener((v) -> {
+
+            Intent intent = new Intent(MainActivity.this, CreaMemox.class);
+            startActivity(intent);
+        });
     }
 
 }

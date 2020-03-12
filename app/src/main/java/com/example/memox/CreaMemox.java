3b57b4e8d056002ce45fee562c98084057e71231
Frédéric <frederic.canaud@etu.unilim.fr>
2020-03-12 10:37:53 +0100

    Ajout callback

--- app/src/main/java/com/example/memox/CreaMemox.java
+++ app/src/main/java/com/example/memox/CreaMemox.java
@@ -1,8 +1,11 @@
 package com.example.memox;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class CreaMemox extends Activity {
@@ -22,8 +25,17 @@ public class CreaMemox extends Activity {
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(CreaMemox.this, android.R.layout.simple_spinner_dropdown_item, selectItems);
         //set the spinners adapter to the previously created one.
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dropdown.setAdapter(adapter);
 
+        final Button back = findViewById((R.id.back));
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreaMemox.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
